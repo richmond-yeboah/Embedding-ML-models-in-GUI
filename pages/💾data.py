834a0d5477514ccf61cb30881a21c1123e4f8f73
Richmond Yeboah <richmondyeboah299@gmail.com>
2024-08-10 19:15:55 +0000

    make necessary changes

--- pages/💾data.py
+++ pages/💾data.py
@@ -4,3 +4,10 @@ import numpy as np
 
 
 st.title("This is the data page")
+
+@st.cache_data(show_spinner='Loading data')
+def load_data():
+    df = pd.read_csv('https://github.com/richmond-yeboah/Customer-Churn-Prediction/raw/main/Data/Telco.csv')
+    return df
+
+df = load_data().head(100)
