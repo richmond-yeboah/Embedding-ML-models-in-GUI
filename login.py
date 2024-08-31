371045dce8ba7a641711ba2dde2022550bbe01d1
Richmond Yeboah <richmondyeboah299@gmail.com>
2024-08-31 11:14:03 +0000

    make changes

--- login.py
+++ login.py
@@ -4,27 +4,31 @@ import yaml
 from yaml.loader import SafeLoader
 
 def login_user():
+    # Load the configuration from config.yaml
     with open('./config.yaml') as file:
         config = yaml.load(file, Loader=SafeLoader)
-        authenticator = stauth.Authenticate(
-            config['credentials'],
-            config['cookie']['name'],
-            config['cookie']['key'],
-            config['cookie']['expiry_days']
-        )
-    col1, col2, col3 = st.columns([.3,.4,.3])
+
+    # Initialize the authenticator with credentials and cookie settings
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days']
+    )
+
+    # Layout for login
+    col1, col2, col3 = st.columns([.3, .4, .3])
     with col2:
-        
-        authenticator.login()
-        
-        if st.session_state["authentication_status"] == True:
+        name, authentication_status, username = authenticator.login('Login', 'main')
+
+        if authentication_status:
             with st.sidebar:
-                authenticator.logout()
-        elif st.session_state["authentication_status"] is False:
+                authenticator.logout('Logout', 'sidebar')
+            st.success(f'Welcome {name}')
+        elif authentication_status is False:
             st.error('Username/password is incorrect')
-        elif st.session_state["authentication_status"] is None:
+        elif authentication_status is None:
             st.warning('Please enter your username and password')
 
-    
 if __name__ == '__main__':
     login_user()
