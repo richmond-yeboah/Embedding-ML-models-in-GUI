033996dcf39c83372a862df5546b59b231cc242f
Richmond Yeboah <richmondyeboah299@gmail.com>
2024-08-31 13:35:38 +0000

    add changes

--- login.py
+++ login.py
@@ -18,9 +18,11 @@ def login_user():
         
         authenticator.login()
         
-        if (st.session_state["authentication_status"] == True) or (st.session_state["authentication_status"] == False):
+        if st.session_state["authentication_status"] == True:
             with st.sidebar:
                 authenticator.logout()
+        elif st.session_state["authentication_status"] is False:
+            st.error('Username/password is incorrect')
         elif st.session_state["authentication_status"] is None:
             st.warning('Please enter your username and password')
 
